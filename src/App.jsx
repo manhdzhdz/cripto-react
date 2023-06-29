829b40ed3765f9a39e522570b21d27d5e95c93d5
m@hdz <manhdz@hotmail.com>
2023-06-29 13:05:47 -0600

    Se creo la lista de cripto  segrago axios y se creo el archivo en valiable

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-function App() {
-   
+import { useState } from "react";
+import { useEffect } from "react";
+// importar axios 
+import axios from "axios";
 
+function App() {
+  //SE CREA UNA CONSTANTE QUE CONTENGA LA VARIABLE DE ENTORN
+  const API_URL=import.meta.env.VITE_API_URL
+
+  const [criptos, setCriptos] = useState();
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)//HACE REFERENCIA A LA CONSTANTE
+      // .then((resp) => resp.json())   AXIOS YA HACE LA CONVERSIÓN A JSON
+      .then((data) => {
+        console.log(data)
+        setCriptos(data.data.data);
+      })
+      .catch(() => {
+        console.error("La petición no funcina!");
+      });
+  }, []);
+
+  // agregar un louder para el tiempo de espera de los datos a la hora de la consulta
+  if(!criptos)return <span>Cargando...</span>
+  
   return (
     <>
-      <h1>Hola</h1>
+      <h1>LISTA DE CRIPTO MONEDAS</h1>
+      <ol>
+        {
+          criptos.map(({ id, name, priceUsd }) => (
+          <li key={id}>
+            Nombre: {name} Precio: {priceUsd}
+          </li>))
+        }
+      </ol>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
